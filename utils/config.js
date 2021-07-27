4bc6721c585af2f49e7901104a399825324f9c2f
liukang <liukang@ava.com.cn>
2021-07-27 11:47:27 +0800

    feat: init project

---
+++ utils/config.js
@@ -0,0 +1,25 @@
+/*
+ * 
+ * 微慕小程序开源版
+ * author: jianbo
+ * organization: 微慕  www.minapper.com
+ * github:    https://github.com/iamxjb/winxin-app-watch-life.net
+ * 技术支持微信号：iamxjb
+ * 开源协议：MIT
+ * Copyright (c) 2017  微慕 https://www.minapper.com All rights reserved.
+ */
+
+
+
+//配置域名,域名只修改此处。
+//如果wordpress没有安装在网站根目录请加上目录路径,例如："www.watch-life.net/blog"
+// var DOMAIN = "www.watch-life.net";
+var DOMAIN = "youtube.peacekang.com";
+var WEBSITENAME="油管精选"; //网站名称
+var PAGECOUNT='10'; //每页文章数目
+export default {
+  getDomain: DOMAIN,
+  getWebsiteName: WEBSITENAME,  
+  getPageCount: PAGECOUNT
+ 
+}
